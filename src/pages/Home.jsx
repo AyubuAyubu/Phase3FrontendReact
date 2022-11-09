dff5703c15e9aee080b5ec5f1f0e96d636cc6ede
Bodipoh <berylodipoh@gmail.com>
2022-11-09 15:07:13 +0300

    origin

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Hello About</h1>
+      <h1>WELCOME TO xCONTACT SYSTEM</h1>
+      <h2>We save your contact in good way</h2>
+    
+      <button className="join us">JOIN US</button>
     </div>
   );
 };
 export default Home;
+
