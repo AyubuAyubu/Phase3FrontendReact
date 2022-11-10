f2cb5fe2cc3dedfb23642c2055d591165bd200f3
Bodipoh <berylodipoh@gmail.com>
2022-11-10 16:30:58 +0300

    Updated home and Course

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home";
+import Home from "./pages/home/Home";
 import Student from "./pages/Student";
-import Course from "./pages/Course";
+import Course from "./pages/course/Course";
 
 
 
@@ -26,3 +26,5 @@ const App = () => {
 };
 
 export default App;
+
+
